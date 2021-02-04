47175ed43d37665c7dbf02de489d6da3db6ad829
Nataliesmyth <nataliesmyth11@gmail.com>
2021-02-03 19:36:48 -0500

    allow admin users to edit post and delete posts

--- verbatim/blog/admin.py
+++ verbatim/blog/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Post 
+
+# add models to admin site
+admin.site.register(Post)
